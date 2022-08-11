2b90e4cc4672117d91cf2a5ff587a563cd0e0145
cos-overclock <60503661+cos-overclock@users.noreply.github.com>
2022-08-11 13:27:29 +0900

    add display svg

--- lib/game/game_page.dart
+++ lib/game/game_page.dart
@@ -1,4 +1,5 @@
 import 'package:flame/game.dart';
+import 'package:flame_svg/flame_svg.dart';
 import 'package:flutter/material.dart';
 
 import 'widget/button_component.dart';
@@ -39,16 +40,19 @@ class GameStartWidget extends StatelessWidget {
 }
 
 class GameClass with Game {
+  late Svg trumpImageInstance;
+
   @override
-  Future<void> onLoad() async {}
+  Future<void> onLoad() async {
+    trumpImageInstance = await loadSvg('images/spade_4.svg');
+  }
 
   @override
   void render(Canvas canvas) {
-    // TODO: implement render
+    trumpImageInstance.renderPosition(
+        canvas, Vector2(100, 100), Vector2.all(300));
   }
 
   @override
-  void update(double dt) {
-    // TODO: implement update
-  }
+  void update(double dt) {}
 }
